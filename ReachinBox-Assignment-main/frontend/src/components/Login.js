bf6072cb0a71d313f5c0e2863246ebc619fff54e
Ranjithkarthi <81366510+Ranjithkarthi@users.noreply.github.com>
2024-08-26 17:24:31 +0530

    Update Login.js

--- ReachinBox-Assignment-main/frontend/src/components/Login.js
+++ ReachinBox-Assignment-main/frontend/src/components/Login.js
@@ -58,7 +58,7 @@ const Login = () => {
 
     // Function to handle Google login
     const handleLogin = () => {
-        window.location.href = 'https://localhost:3000';
+        window.location.href = 'https://reach-ai-frontend-assignment.onrender.com';
     };
 
     // Handle token if present in the URL after login redirect
